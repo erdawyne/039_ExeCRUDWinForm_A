a520a93de4dd69c80eed15220e9967ddb9a987ea
ERDA WYNE <erdawynee1@gmail.com>
2022-06-15 16:39:24 +0700

    membuat kelas

---
+++ Exercise_CRUD/student.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_CRUD
+{
+    class student
+    {
+        public string Nama { get; set; }
+        public string No { get; set; }
+        public string JK { get; set; }
+        public string Kelas { get; set; }
+
+        public student(string nama, string no, string jK, string kelas)
+        {
+            Nama = nama;
+            No = no;
+            JK = jK;
+            Kelas = kelas;
+        }
+    }
+}
